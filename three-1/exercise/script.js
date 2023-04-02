f12b9f7b2af24237893e49af1b91ab558af96bb7
Ramla Jama <75176930+ramla199@users.noreply.github.com>
2023-04-02 22:18:37 +0000

    "first threejs exercise"

--- three-1/exercise/script.js
+++ three-1/exercise/script.js
@@ -16,4 +16,17 @@ const sizes = {
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
 scene.add(camera)
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// ...
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
